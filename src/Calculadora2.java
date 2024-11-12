b05556b9686cb9c16e83af8fb7ebfeb65e50b49e
leonardo rossine <rosinleonardo07@gmail.com>
2024-11-11 22:06:34 -0300

    FG

---
+++ src/Calculadora2.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Calculadora2 {
+    private JTextField textField1;
+    private JButton btn1;
+    private JButton btnadição;
+    private JButton btn4;
+    private JButton btn7;
+    private JButton btn2;
+    private JButton btn5;
+    private JButton btn8;
+    private JButton btn3;
+    private JButton btn6;
+    private JButton btn9;
+    private JButton btn0;
+    private JButton btnsubtrair;
+    private JButton btnmultiplicacao;
+    private JButton btnigual;
+    private JButton btndivisão;
+    private JButton btnapagar;
+}
